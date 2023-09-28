3d47489d3240ad74a2bf76ad33b75f1904fc2f70
Remysaurus <48845480+Remysaurus@users.noreply.github.com>
2023-09-28 16:34:53 -0700

    Need a windows computer 

--- remy/Programming Training/src/main/java/frc/robot/commands/ArcadeElevator.java
+++ remy/Programming Training/src/main/java/frc/robot/commands/ArcadeElevator.java
@@ -4,34 +4,24 @@
 
 package frc.robot.commands;
 
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
 
-
-public class ArcadeDrive extends CommandBase {
-  private static final class Config{
-    public static final int kLeftYAxis = 1;
-    public static final int kRightXAxis = 4;
-    public static final double kSpeedMultiplier = 0.5;
-    public static final double kTurnMultiplier = 0.5;
+public class ArcadeElevator extends CommandBase {
+  public static final class Config{
+    public static final int kElevatorYAxis = 2;
     public static final double kElevatorMultiplier = Math.PI/26;
-
-
   }
   private Joystick m_joystick;
-  private Drivetrain m_drivetrain;
+  private Elevator m_elevator;
+  /** Creates a new ArcadeElevator. */
 
-
-  /** Creates a new ArcadeDrive. */
-  public ArcadeDrive(Joystick joystick, Drivetrain drivetrain) {
+  public ArcadeElevator(Joystick joystick, Elevator elevator) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_joystick = joystick;
-    m_drivetrain = drivetrain;
-    addRequirements(m_drivetrain);
-
-    
+    m_elevator = elevator;
   }
 
   // Called when the command is initially scheduled.
@@ -42,23 +32,13 @@ public class ArcadeDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double speed = m_joystick.getRawAxis(Config.kRightXAxis) * Config.kSpeedMultiplier;
-    double turn  = m_joystick.getRawAxis(Config.kLeftYAxis) * Config.kTurnMultiplier;
-    double left = speed + turn;
-    double right = speed - turn;
-    m_drivetrain.setLeftSpeed(left);
-    m_drivetrain.setRightSpeed(right);
-
-    
+    m_elevator.SetSpeed(m_joystick.getRawAxis(Config.kElevatorYAxis) * Config.kElevatorMultiplier);
   }
-
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_drivetrain.setLeftSpeed(0);
-    m_drivetrain.setRightSpeed(0);
-    
+    m_elevator.SetSpeed(0);
   }
 
   // Returns true when the command should end.
